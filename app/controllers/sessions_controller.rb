43287814b736e44163384af0ed05f2cf13a1a69f
kiiiichiii <orrangee@icloud.com>
2021-04-22 21:39:53 +0900

    change6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(nickname: params[:session][:nickname])
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to ("/logintop")
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,8 +44,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation, :grade, :expert, :nickname)
+      params.require(:user).permit(:password,:password_confirmation, :grade, :expert, :nickname)
     end
 
     def correct_user
